1068ea36488ce9b337c31a2f3a61bd395bf9b100
Abram Booth <boothaa@gmail.com>
2022-02-03 15:08:58 -0500

    source_ref analysis

--- morningwhale/metrics.py
+++ morningwhale/metrics.py
@@ -7,7 +7,7 @@ class LogEvent(metrics.Metric):
     func_name = metrics.Keyword()
     log_level = metrics.Keyword()
     log_message_unformatted = metrics.Keyword()
-    source_ref = metrics.Text()
+    source_ref = metrics.Text(analyzer='simple')
 
     class Meta:
         source = metrics.MetaField(enabled=True)
