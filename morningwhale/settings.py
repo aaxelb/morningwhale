17807c9b08c5e1ff91f4f8232fc9c8bb250c477e
Abram Booth <boothaa@gmail.com>
2022-02-03 15:44:21 -0500

    get elasticsearch host from env var

--- morningwhale/settings.py
+++ morningwhale/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -128,7 +129,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 # settings for django-elasticsearch-metrics
 ELASTICSEARCH_DSL = {
     'default': {
-        'hosts': 'elasticsearch6:9200',  # TODO env var
+        'hosts': os.environ.get('ELASTICSEARCH_HOST', 'mw_elasticsearch:9200'),
     },
 }
 ELASTICSEARCH_METRICS_DATE_FORMAT = '%Y.%m'
